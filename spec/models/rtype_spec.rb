10312f52de22458731c723dfac8f96a9fb66b4e5
Danny <barakadan421@gmail.com>
2023-03-25 17:04:49 +0300

    Add invalid test for the rtype

--- spec/models/rtype_spec.rb
+++ spec/models/rtype_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
+require 'support/factory_bot'
 
 RSpec.describe Rtype, type: :model do
   describe "validations" do
     it "is valid with a name" do
       rtype = Rtype.new(name: "wifi")
       expect(rtype).to be_valid
+    end
+
+    it "is invalid without a name" do
+      rtype = Rtype.new(name: nil)
+      expect(rtype).to_not be_valid
     end
   end
 end
