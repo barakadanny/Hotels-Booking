06b49e8285c6d00af868aa631336abd319393548
Danny <barakadan421@gmail.com>
2023-03-30 20:05:42 +0300

    Update room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  belongs_to :hotel, dependent: :destroy
+  belongs_to :hotel
   has_many :room_photos, dependent: :destroy
   has_one :rtype, through: :room_type, dependent: :destroy
   has_many :services, through: :room_services , dependent: :destroy
