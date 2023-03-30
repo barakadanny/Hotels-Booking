2ec6b6bee1251a1c00c6302fa3d3825d34763e4b
Danny <barakadan421@gmail.com>
2023-03-30 17:34:38 +0300

    rooms controller route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :hotels
+      resources :hotels do
+        resources :rooms, only: [:create]
+      end
+      resources :rooms, only: [:index, :destroy, :update, :show]
     end
   end
 end
