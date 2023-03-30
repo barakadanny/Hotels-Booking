567cb4789f9bc7dc97a4e3edbed5a22bbc913b03
Danny <barakadan421@gmail.com>
2023-03-30 18:21:12 +0300

    Update room model for destroy dependent

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  belongs_to :hotel
+  belongs_to :hotel, dependent: :destroy
   has_many :room_photos, dependent: :destroy
   has_one :rtype, through: :room_type, dependent: :destroy
   has_many :services, through: :room_services , dependent: :destroy
