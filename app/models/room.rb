68e33399e3eee349e8d05d238b7187ba85f8919d
Danny <barakadan421@gmail.com>
2023-04-01 22:51:55 +0300

    Update Room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,10 @@
 class Room < ApplicationRecord
   belongs_to :hotel
   has_many :service_rooms
-  has_many :room_photos, dependent: :destroy
-  has_one :rtype, through: :room_type, dependent: :destroy
   has_many :services, through: :service_rooms , dependent: :destroy
+  has_one :room_type
+  has_one :rtype, through: :room_type, dependent: :destroy
+  has_many :room_photos, dependent: :destroy
 
   validates :description, presence: true
   validates :price_per_night, presence: true, numericality: { greater_than_or_equal_to: 0 }
